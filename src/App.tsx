8174532d3ab18ee23f584e2132f9c802c8935784
vishesh0123 <visheshsachdev04@gmail.com>
2023-06-05 21:50:27 +0530

    polybase authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useLoaderData
+} from 'react-router-dom'
+import './App.css'
+import { PolybaseProvider, AuthProvider } from '@polybase/react'
+import { Polybase } from '@polybase/client'
+import { Auth } from '@polybase/auth'
+import PolybaseAuth from './components/PolybaseAuth'
 
-function App() {
+const polybase = new Polybase()
+const auth = new Auth()
+
+const router = createBrowserRouter([
+  {
+    path: '/app',
+    loader: () => ({ message: 'SyncP2P: Authentication' }),
+    Component (): JSX.Element {
+      const data = useLoaderData() as { message: string }
+      return (
+        <>
+          <h1>{data.message}</h1>
+          <PolybaseAuth />
+        </>
+      )
+    }
+  }
+])
+
+function App (): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <PolybaseProvider polybase={polybase}>
+      <AuthProvider auth={auth} polybase={polybase}>
+        <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
+      </AuthProvider>
+    </PolybaseProvider>
+  )
 }
 
-export default App;
+export default App
